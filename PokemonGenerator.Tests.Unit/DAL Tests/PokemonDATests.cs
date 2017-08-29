fa7fd89c96135cb1e334e4a8a471e50d130adc39
Justin Robb <reptarsrage@gmail.com>
2017-08-29 00:55:35 -0700

    Added unit test project
    - With DAL Tests

---
+++ PokemonGenerator.Tests.Unit/DAL Tests/PokemonDATests.cs
@@ -0,0 +1,162 @@
+﻿using NUnit.Framework;
+using PokemonGenerator.DAL;
+using PokemonGenerator.Models;
+using System.Linq;
+
+namespace PokemonGenerator.Tests.Unit
+{
+    [TestFixture]
+    public class PokemonDATests
+    {
+        [Test]
+        public void GetPossiblePokemonTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var pokemon = da.GetPossiblePokemon(100, Enumerations.Entropy.Low);
+            Assert.IsNotNull(pokemon);
+            Assert.Greater(pokemon.Count(), 0);
+        }
+
+        [Test]
+        public void GetPossiblePokemonValuesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var pokemon = da.GetPossiblePokemon(100, Enumerations.Entropy.Low);
+            pokemon.ToList().ForEach(Assert.NotZero);
+        }
+
+        [Test]
+        public void GetMovesForPokemonTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var moves = da.GetMovesForPokemon(62, 100);
+            Assert.IsNotNull(moves);
+            Assert.Greater(moves.Count(), 0);
+        }
+
+        [Test]
+        public void GetMovesForPokemonValuesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var moves = da.GetMovesForPokemon(62, 100);
+            moves.ToList().ForEach(AssertPokemonMoveSetResult);
+        }
+
+        [Test]
+        public void GetRandomMovesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var moves = da.GetRandomMoves(0, 150);
+            Assert.IsNotNull(moves);
+            Assert.Greater(moves.Count(), 0);
+        }
+
+        [Test]
+        public void GetRandomMovesValuesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var moves = da.GetRandomMoves(0, 150);
+            moves.ToList().ForEach(AssertPokemonMoveSetResult);
+        }
+
+        [Test]
+        public void GetTeamBaseStatsTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var baseStats = da.GetTeamBaseStats(new PokeList(4)
+            {
+                Species = new byte[] { 0, 1, 2, 3 }
+            });
+            Assert.IsNotNull(baseStats);
+            Assert.Greater(baseStats.Count(), 0);
+        }
+
+        [Test]
+        public void GetTeamBaseStatsValuesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var baseStats = da.GetTeamBaseStats(new PokeList(4)
+            {
+                Species = new byte[] { 0, 1, 2, 3 }
+            });
+            foreach (var stat in baseStats)
+            {
+                Assert.NotZero(stat.Id);
+                Assert.NotZero(stat.Hp);
+                Assert.NotZero(stat.Attack);
+                Assert.NotZero(stat.Defense);
+                Assert.NotZero(stat.SpAttack);
+                Assert.NotZero(stat.SpDefense);
+                Assert.NotZero(stat.Speed);
+
+                Assert.IsNotNull(stat.GrowthRate);
+                Assert.IsNotEmpty(stat.GrowthRate);
+
+                Assert.IsNotNull(stat.Identifier);
+                Assert.IsNotEmpty(stat.Identifier);
+
+                Assert.IsNotNull(stat.Types);
+                Assert.IsNotEmpty(stat.Types);
+            }
+        }
+
+        [Test]
+        public void GetTMsTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var tms = da.GetTMs();
+            Assert.IsNotNull(tms);
+            Assert.Greater(tms.Count(), 0);
+        }
+
+        [Test]
+        public void GetTMsValueTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var tms = da.GetTMs();
+            tms.ToList().ForEach(Assert.NotZero);
+        }
+
+        [Test]
+        public void GetWeaknessesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var weaknesses = da.GetWeaknesses("fire");
+            Assert.IsNotNull(weaknesses);
+            Assert.Greater(weaknesses.Count(), 0);
+        }
+
+        [Test]
+        public void GetWeaknessesValuesTest()
+        {
+            var da = new PokemonDA("ThePokeBase");
+            var weaknesses = da.GetWeaknesses("fire");
+            weaknesses.ToList().ForEach(Assert.IsNotNull);
+            weaknesses.ToList().ForEach(Assert.IsNotEmpty);
+        }
+
+        private void AssertPokemonMoveSetResult(PokemonMoveSetResult move)
+        {
+            Assert.LessOrEqual(move.Level, 100, "Level");
+            Assert.GreaterOrEqual(move.Level, 0, "Level");
+            Assert.NotZero(move.MoveId, "Move Id");
+            Assert.GreaterOrEqual((int)(move?.Power ?? 0), 0, "Power");
+            Assert.GreaterOrEqual((int)(move?.Pp ?? 0), 0, "PP");
+
+            Assert.IsNotNull(move.MoveName, "Move Name");
+            Assert.IsNotEmpty(move.MoveName, "Move Name");
+
+            Assert.IsNotNull(move.Type, "Move Type");
+            Assert.IsNotEmpty(move.Type, "Move Type");
+
+            // damageType can be null when move does no damage
+            Assert.IsNotEmpty(move.DamageType, "Move Damage Type");
+
+            // learnType can be null
+            Assert.IsNotEmpty(move.LearnType, "Move Learn Type");
+
+            Assert.IsNotNull(move.Effect, "Move Effect");
+            Assert.IsNotEmpty(move.Effect, "Move Effect");
+        }
+    }
+}
---
+++ PokemonGenerator.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PokemonGenerator.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PokemonGenerator.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e087623-fa33-4e1c-a310-40d92d94f67a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
